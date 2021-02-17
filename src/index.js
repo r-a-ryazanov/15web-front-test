185c106922a8689e28d7d1937938d47612748068
Roman Ryazanov <r.a.ryazanov@yandex.ru>
2021-02-17 14:11:27 +0300

    Оставить коментарии

--- src/index.js
+++ src/index.js
@@ -11,34 +11,45 @@ const buttonRandomPosition = page.querySelector(".navigation__button_position_ra
 const buttonNextPosition = page.querySelector(".navigation__button_position_next");
 const buttonLastPosition = page.querySelector(".navigation__button_position_last");
 let numberOfLastComics;
-function handleComics(comicsData){
+
+//--------Функция обработки данных, полученных от api---------------
+function handleComics(comicsData) {
   comicsTitle.textContent = comicsData.title;
   comicsImage.src = comicsData.img;
   comicsImage.alt = comicsData.alt;
   comicsTranscription.textContent = comicsData.transcript;
   comicsDate.textContent = `${comicsData.day}.${comicsData.month}.${comicsData.year}`;
-  if(Number(window.location.search.slice(1)) !== comicsData.num) window.location.search = `?${comicsData.num}`;
-   disabledButtonsFunc();
- }
- function handleError(err){
+  if (Number(window.location.search.slice(1)) !== comicsData.num) window.location.search = `?${comicsData.num}`;
+  disabledButtonsFunc();
+}
+
+//--------Функция обработки ошибок---------------
+function handleError(err) {
   comicsTitle.textContent = err;
   comicsImage.src = '';
   comicsImage.alt = '';
   comicsTranscription.textContent = '';
   comicsDate.textContent = '';
-   disabledButtonsFunc();
- }
-getComics(0,(data) =>{
+  disabledButtonsFunc();
+}
+
+//--------Определение максимального колличества комиксов---------------
+getComics(0, (data) => {
   numberOfLastComics = data.num;
 }, handleError);
+//--------Отключение кнопок меню перед загрузкой---------------
 disabledButtonsFunc();
-if(Number(window.location.search.slice(1))) {
-  getComics(Number(window.location.search.slice(1)),handleComics, handleError);
-}else{
+//--------Загрузка комикса по параметру url---------------
+if (Number(window.location.search.slice(1))) {
+  getComics(Number(window.location.search.slice(1)), handleComics, handleError);
+} else if (!window.location.search) {
+  getComics(0, handleComics, handleError);
+} else {
   handleError('Неправильно указан номер комикса!');
 }
 
-function disabledButtonsFunc(){
+//--------Функция включения/выключения кнопок---------------
+function disabledButtonsFunc() {
   buttonFirstPosition.toggleAttribute('disabled');
   buttonPrevPosition.toggleAttribute('disabled');
   buttonRandomPosition.toggleAttribute('disabled');
@@ -46,35 +57,44 @@ function disabledButtonsFunc(){
   buttonLastPosition.toggleAttribute('disabled');
 }
 
-
-function goToFirstComics(){
+//--------Функция обработки нажатия на кнопку "Первый"---------------
+function goToFirstComics() {
   disabledButtonsFunc();
-  getComics(1,handleComics, handleError);
+  getComics(1, handleComics, handleError);
 }
-function goToPrevComics(){
-  if(Number(window.location.search.slice(1)) >1) {
-    getComics(Number(window.location.search.slice(1)) - 1,handleComics, handleError);
+
+//--------Функция обработки нажатия на кнопку "Предыдущий"---------------
+function goToPrevComics() {
+  if (Number(window.location.search.slice(1)) > 1) {
+    getComics(Number(window.location.search.slice(1)) - 1, handleComics, handleError);
   } else {
     handleError('Вы просматриваете первый комикс!');
   }
   disabledButtonsFunc();
 }
-function goToRandomComics(){
-  getComics(Math.floor((Math.random()*numberOfLastComics) + 1),handleComics, handleError);
+
+//--------Функция обработки нажатия на кнопку "Случайный"---------------
+function goToRandomComics() {
+  getComics(Math.floor((Math.random() * numberOfLastComics) + 1), handleComics, handleError);
   disabledButtonsFunc();
 }
-function goToNextComics(){
-  if(Number(window.location.search.slice(1)) < numberOfLastComics) {
-    getComics(Number(window.location.search.slice(1)) + 1,handleComics, handleError);
+
+//--------Функция обработки нажатия на кнопку "Следующий"---------------
+function goToNextComics() {
+  if (Number(window.location.search.slice(1)) < numberOfLastComics) {
+    getComics(Number(window.location.search.slice(1)) + 1, handleComics, handleError);
   } else {
     handleError('Вы просматриваете последний комикс!');
   }
   disabledButtonsFunc();
 }
-function goToLastComics(){
+//--------Функция обработки нажатия на кнопку "Последний"---------------
+function goToLastComics() {
   disabledButtonsFunc();
-  getComics(0,handleComics, handleError);
+  getComics(0, handleComics, handleError);
 }
+
+//--------Включение слушателей---------------
 buttonFirstPosition.addEventListener('click', goToFirstComics);
 buttonPrevPosition.addEventListener('click', goToPrevComics);
 buttonRandomPosition.addEventListener('click', goToRandomComics);
--- src/script/api.js
+++ src/script/api.js
@@ -1,23 +1,24 @@
-function getDefinedComics(url, handleComics, handleError){
+//----------Функция для загрузки комикса------------------
+function getDefinedComics(url, handleComics, handleError) {
   fetch(url)
-  .then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-    return res.status === 404 ? Promise.reject(`Комикс не найден.`): Promise.reject(`Произошла ошибка. Попробуйте позже!`);
-  })
-  .then((result) => {
-    handleComics(result);
-  })
-  .catch((err) => {
-    handleError(err);
-  });
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return res.status === 404 ? Promise.reject(`Комикс не найден.`) : Promise.reject(`Произошла ошибка. Попробуйте позже!`);
+    })
+    .then((result) => {
+      handleComics(result);
+    })
+    .catch((err) => {
+      handleError(err);
+    });
 }
-
-export default function getComics(numberOfComics, handleComics, handleError){
-  if(numberOfComics>0){
-    getDefinedComics(`http://xkcd.com/${numberOfComics}/info.0.json`, handleComics, handleError);
-  } else{
-    getDefinedComics(`http://xkcd.com/info.0.json`, handleComics, handleError);
+//----------Функция для экспорта и формирования url------------------
+export default function getComics(numberOfComics, handleComics, handleError) {
+  if (numberOfComics > 0) {
+    getDefinedComics(`https://xkcd.com/${numberOfComics}/info.0.json`, handleComics, handleError);
+  } else {
+    getDefinedComics(`https://xkcd.com/info.0.json`, handleComics, handleError);
   }
 }
